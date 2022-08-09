a53828420718264a33562b3edcb9e0c4803e27cf
brainconnect93 <brainconnect0@gmail.com>
2022-08-09 20:13:52 +0100

    WOrking on the navbar section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App() {
-	return (
-		null
-	);
+import React from 'react';
+
+const App = () => {
+  return (
+	<>
+	  <h1>I am Starting my own personal portfolio project</h1>
+	</>  
+  );
 }
 
 export default App;
