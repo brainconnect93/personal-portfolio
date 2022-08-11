53680294d86e0b6dd333158f59d189bb3c5bc454
brainconnect93 <brainconnect0@gmail.com>
2022-08-11 21:06:26 +0100

    Start the head section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Head from './components/Head/Head';
 
-const App = () => {
-  return (
-	<>
-	  <Navbar />
-	  <Head />
-	</>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Head />
+  </>
+);
 
 export default App;
--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './head.css';
 
-const Head = () => {
-  return (
-    <div className="card"> Head</div>
-  )
-}
+const Head = () => (
+  <div className="container-fluid head">
+    <div className="container head-content">
+      <p>
+        I am Afolabi Akorede a software developer, with years of experience on
+        JavaScript, React, Redux, Tailwind, Ruby, Ruby on rail and many more. I
+        can help you build a product, feature or website Look through some of my
+        work and experience! If you like what you see and have a project you
+        need to work on, dont hestiate to contact me.
+      </p>
+      <button type="button" className="btn-details">View Details</button>
+    </div>
+  </div>
+);
 
 export default Head;
