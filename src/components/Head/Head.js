fb70a195e24bb090041dc065cc33dc605e86ca11
brainconnect93 <brainconnect0@gmail.com>
2022-08-12 12:38:47 +0100

    complete the head section of the project with the styling

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './head.css';
+import Typewriter from 'typewriter-effect';
 
 const Head = () => (
   <div className="container-fluid head">
     <div className="container head-content">
+      <h1 className="head-text">
+        <Typewriter
+          options={{
+            strings: [
+              'Full-Stack developer',
+              'Fron-End developer',
+              'JavaScript and React',
+              'UI/UX Design',
+              'Customer Service',
+            ],
+            autoStart: true,
+            loop: true,
+            delay: 10,
+          }}
+        />
+      </h1>
       <p>
         I am Afolabi Akorede a software developer, with years of experience on
         JavaScript, React, Redux, Tailwind, Ruby, Ruby on rail and many more. I
@@ -11,7 +28,9 @@ const Head = () => (
         work and experience! If you like what you see and have a project you
         need to work on, dont hestiate to contact me.
       </p>
-      <button type="button" className="btn-details">View Details</button>
+      <button type="button" className="btn-details">
+        View Details
+      </button>
     </div>
   </div>
 );
