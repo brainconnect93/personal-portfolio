0c87c4beb19b179dfff311b8b96bbb85361c501e
brainconnect93 <brainconnect0@gmail.com>
2022-08-12 18:10:16 +0100

    Add some styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,8 @@
 import React from "react";
 import "./about.css";
 import dubai from "../../Images/dubai.jpg";
+import { FaReact } from "react-icons/fa";
+import { SiJavascript, SiRubyonrails } from "react-icons/si";
 
 const About = () => (
   <div className="container about-sec">
@@ -22,6 +24,55 @@ const About = () => (
             Hello I&apos;m a developer! I can help you build a product, feature
             or website. Let me be your plug, don&apos;t hestiate to contact me.
           </p>
+
+          <div className="about-cards">
+            <div className="about-items">
+              <div className="about-icon">
+                <p>
+                  <SiRubyonrails size={50} />
+                </p>
+              </div>
+              <div>
+                <h6>Ruby on rail</h6>
+                <p>
+                  Rails is a full-stack framework. It ships with all the tools
+                  needed to build amazing web apps on both the front and back
+                  end
+                </p>
+              </div>
+            </div>
+
+            <div className="about-items">
+              <div className="about-icon">
+                <p>
+                  <FaReact size={40} />
+                </p>
+              </div>
+              <div>
+                <h6>React</h6>
+                <p>
+                  React is a JavaScript library for building user interfaces.
+                  Declarative: React makes it painless to create interactive
+                  UIs.
+                </p>
+              </div>
+            </div>
+
+            <div className="about-items">
+              <div className="about-icon">
+                <p>
+                  <SiJavascript size={40} />
+                </p>
+              </div>
+              <div>
+                <h6>JavaScript</h6>
+                <p>
+                  JavaScript is the world&apos;s most popular programming
+                  language. JavaScript is the programming language of the Web
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
