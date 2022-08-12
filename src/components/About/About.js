06b95cfc844e201c6248a08bebcb8c512cc45ae8
brainconnect93 <brainconnect0@gmail.com>
2022-08-12 13:11:49 +0100

    Add the About section to the project

---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './about.css';
+
+const About = () => (
+  <div className="container about-sec">
+    <div className="row">
+      <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+        About
+      </div>
+
+      <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+        Coming home
+      </div>
+
+    </div>
+  </div>
+);
+
+export default About;
