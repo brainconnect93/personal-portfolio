83255096e2beb8ecb16c007fd1cfab73fcbb31af
brainconnect93 <brainconnect0@gmail.com>
2022-08-12 22:23:23 +0100

    Add the service folder with files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Head from './components/Head/Head';
 import About from './components/About/About';
+import Service from './components/Service/Service';
 
 const App = () => (
   <>
     <Navbar />
     <Head />
     <About />
+    <Service />
   </>
 );
 
---
+++ src/components/Service/Service.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './service.css';
+
+const Service = () => (
+  <div>Service</div>
+);
+
+export default Service;
