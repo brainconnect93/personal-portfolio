31583f1dae4a45bbf5dd1a0f8b700a09341d6028
brainconnect93 <brainconnect0@gmail.com>
2022-08-13 01:22:18 +0100

    complete styling of the service section

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -47,6 +47,31 @@ const Service = () => {
       service3: "UI/UX design",
       service4: "React and JavaScript",
     },
+    {
+      id: 7,
+      service1: "Full-Stack web development",
+      service2: "Front-End development",
+      service3: "UI/UX design",
+      service4: "React and JavaScript",
+    },
+    {
+      id: 8,
+      service1: "Full-Stack web development",
+      service2: "Front-End development",
+      service3: "UI/UX design",
+      service4: "React and JavaScript",
+    },
+  ];
+
+  const colors = [
+    "#cd5c5c",
+    "#f7d010",
+    "#e48c6f",
+    "#ab11ce",
+    "#36ff25",
+    "lightblue",
+    "red",
+    "blue"
   ];
   return (
     <div className="container service-section">
@@ -55,26 +80,37 @@ const Service = () => {
         <span className="line"></span>
       </div>
 
-      <div className="row">
-        {serviceList.map((item, index) =>
-          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6" key={index}>
+      <div className="row service-content">
+        {serviceList.map((item, index) => (
+          <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6" key={index}>
             <div className="service">
-              <span className="number">
+              <span
+                className="numbers"
+                style={{ backgroundColor: colors[index] }}
+              >
                 <p>{item.id}</p>
               </span>
 
               <div className="service-list">
-                <p><FcOk size={20} style={{margin: "5px"}}/> {item.service1}</p>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} /> {item.service1}
+                </p>
                 <span className="list-line"></span>
-                <p><FcOk size={20} style={{margin: "5px"}}/> {item.service2}</p>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} /> {item.service2}
+                </p>
                 <span className="list-line"></span>
-                <p><FcOk size={20} style={{margin: "5px"}}/> {item.service3}</p>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} /> {item.service3}
+                </p>
                 <span className="list-line"></span>
-                <p><FcOk size={20} style={{margin: "5px"}}/> {item.service4}</p>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} /> {item.service4}
+                </p>
               </div>
             </div>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
