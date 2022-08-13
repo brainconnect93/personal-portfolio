87c0bc5ab54035b28e9439925d6d99ef2020ff03
brainconnect93 <brainconnect0@gmail.com>
2022-08-13 22:54:10 +0100

    Add the testimonial scetion folder with files

---
+++ src/components/Testimonial/Testimonial.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from 'react';
+import './testimonial.css';
+
+const Testimonial = () => {
+  return (
+    <div className="container testy-section">
+      <div className="section-head">
+        <h5>Testimonial</h5>
+        <span className="line"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
