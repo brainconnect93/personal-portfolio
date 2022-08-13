f4ca109cd972351bafbc6f48d1fa6a5d897a4561
brainconnect93 <brainconnect0@gmail.com>
2022-08-13 23:13:33 +0100

    Add the fake api to the js file

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -3,6 +3,57 @@ import React from 'react';
 import './testimonial.css';
 
 const Testimonial = () => {
+
+    const testimonialData = [
+      {
+        "img": "../../Images/Passport.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/felix.png",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/IMG-20210111-WA0010.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/pexels-antony-trivet-12887434.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/pexels-cottonbro-6970106.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/pexels-david-kwasi-amuzu-5471615.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/pexels-skildring-by-andreas-ellegaard-12871449.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+      {
+        "img": "../../Images/pexels-jairo-david-arboleda-1432237.jpg",
+        "content": "Thanks to Microverse for given me the opportunity to learn and become certified",
+        "title": "",
+        "name": "",
+      },
+    ]
   return (
     <div className="container testy-section">
       <div className="section-head">
