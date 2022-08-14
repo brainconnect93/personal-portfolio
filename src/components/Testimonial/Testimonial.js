fb38a824a4edeea3483173d2077212de9c59ae10
brainconnect93 <brainconnect0@gmail.com>
2022-08-14 06:30:18 +0100

    Extract image from online source

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -6,55 +6,55 @@ const Testimonial = () => {
 
     const testimonialData = [
       {
-        "img": "../../Images/Passport.jpg",
+        "image": "https://avatars.githubusercontent.com/u/78634841?v=4",
         "content": "Thanks to Microverse for given me the chnace to learn and become certified",
         "name": "Afolabi Akorede",
         "title": "Front-End Developer",
       },
       {
-        "img": "../../Images/felix.png",
-        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
-        "name": "Felix Ouma",
-        "title": "Full-Stack Developer",
-      },
-      {
-        "img": "../../Images/IMG-20210111-WA0010.jpg",
-        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
-        "name": "Afolabi Ikhmot",
-        "title": "Accountant and Data Scientist",
-      },
-      {
-        "img": "../../Images/pexels-antony-trivet-12887434.jpg",
-        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
-        "name": "Anthony Trivet",
-        "title": "React and Javascript developer",
-      },
-      {
-        "img": "../../Images/pexels-cottonbro-6970106.jpg",
-        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
-        "name": "Christabelle Chris",
-        "title": "Software Engineer",
-      },
-      {
-        "img": "../../Images/pexels-david-kwasi-amuzu-5471615.jpg",
-        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
-        "name": "David Kwasi",
-        "title": "Django Developer",
-      },
-      {
-        "img": "../../Images/pexels-skildring-by-andreas-ellegaard-12871449.jpg",
+        "image": "https://images.pexels.com/photos/2962144/pexels-photo-2962144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "content": "Thanks to Microverse for given me the chnace to learn and become certified",
         "name": "Andreas Ellegaard",
         "title": "Ruby Developer",
       },
       {
-        "img": "../../Images/pexels-jairo-david-arboleda-1432237.jpg",
+        "image": "https://avatars.githubusercontent.com/u/16392046?v=4",
         "content": "Thanks to Microverse for given me the chnace to learn and become certified",
-        "name": "Jairo David",
-        "title": "Node Js Developer",
+        "name": "Felix Ouma",
+        "title": "Full-Stack Developer",
       },
       {
-        "img": "../../Images/pexels-pexels-user-12912964.jpg",
+        "image": "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
+        "name": "Anthony Trivet",
+        "title": "React and Javascript developer",
+      },
+      {
+        "image": "https://avatars.githubusercontent.com/u/98928638?v=4",
+        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
+        "name": "Christabelle Youta",
+        "title": "Software Engineer",
+      },
+      {
+        "image": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
+        "name": "David Kwasi",
+        "title": "Django Developer",
+      },
+      {
+        "image": "https://avatars.githubusercontent.com/u/95374858?v=4",
+        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
+        "name": "Grayce Muthui",
+        "title": "Web Developer",
+      },
+      {
+        "image": "https://images.pexels.com/photos/2589409/pexels-photo-2589409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "content": "Thanks to Microverse for given me the chnace to learn and become certified",
+        "name": "Jairo David",
+        "title": "Ruby on rail developer",
+      },
+      {
+        "image": "https://avatars.githubusercontent.com/u/101924220?v=4",
         "content": "Thanks to Microverse for given me the chnace to learn and become certified",
         "name": "Christian Siku",
         "title": "Node Js Developer",
@@ -70,10 +70,13 @@ const Testimonial = () => {
 
       <div className="row testy-content">
         {testimonialData.map((item,index) => (
-          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12" key={index}>
             <div className="testy-cards">
+              <img src={item.image} alt="images" />
               <p>{item.content}</p>
-              <span>{item.name}</span>
+              <p>
+                <span className="testy-name">{item.name}</span>
+              </p>
               <p>{item.title}</p>
             </div>
           </div>
