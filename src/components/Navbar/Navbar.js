4013668567990017ad7467691f626a4794804aa5
brainconnect93 <brainconnect0@gmail.com>
2022-08-15 17:41:33 +0100

    Add the change color in the nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,33 +2,32 @@ import React, { useState } from 'react';
 import './navbar.css';
 
 const Navbar = () => {
-
   const [navBarScroll, setNavBarScroll] = useState(false);
-  
   const seeColorChange = () => {
-    if(window.scrollY>=100) {
+    if (window.scrollY >= 500) {
       setNavBarScroll(true);
-    }else {
+    } else {
       setNavBarScroll(false);
     }
   };
 
   window.addEventListener('scroll', seeColorChange);
-    <>
-      <nav className={navBarScroll ? 'nav-color' : 'nav-menu'}>
-        <ul>
-          <li className="nav-items">Home</li>
-          <li className="nav-items">About</li>
-          <li className="nav-items">Service</li>
-          <li className="nav-items">Career</li>
-          <li className="nav-items">Video</li>
-          <li className="nav-items">Team</li>
-          <li className="nav-items">Testimonial</li>
-          <li className="nav-items">Feedback</li>
-          <li className="nav-items">Contact</li>
-        </ul>
-      </nav>
-    </>;
+
+  return (
+    <nav className={navBarScroll ? 'nav-color nav-menu' : 'nav-menu'}>
+      <ul>
+        <li className="nav-items">Home</li>
+        <li className="nav-items">About</li>
+        <li className="nav-items">Service</li>
+        <li className="nav-items">Career</li>
+        <li className="nav-items">Video</li>
+        <li className="nav-items">Team</li>
+        <li className="nav-items">Testimonial</li>
+        <li className="nav-items">Feedback</li>
+        <li className="nav-items">Contact</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
