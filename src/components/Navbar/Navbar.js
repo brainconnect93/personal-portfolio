25e137b3c5a362ba1a0feb143607ad18a33fb52d
brainconnect93 <brainconnect0@gmail.com>
2022-08-15 15:01:45 +0100

    Add some dynamic function to the navbar section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
-const Navbar = () => (
-  <>
-    <nav className="nav-menu">
-      <ul>
-        <li className="nav-items">Home</li>
-        <li className="nav-items">About</li>
-        <li className="nav-items">Service</li>
-        <li className="nav-items">Career</li>
-        <li className="nav-items">Video</li>
-        <li className="nav-items">Team</li>
-        <li className="nav-items">Testimonial</li>
-        <li className="nav-items">Feedback</li>
-        <li className="nav-items">Contact</li>
-      </ul>
-    </nav>
-  </>
-);
+const Navbar = () => {
+
+  const [navBarScroll, setNavBarScroll] = useState(false);
+  
+  const seeColorChange = () => {
+    if(window.scrollY>=100) {
+      setNavBarScroll(true);
+    }else {
+      setNavBarScroll(false);
+    }
+  };
+
+  window.addEventListener('scroll', seeColorChange);
+    <>
+      <nav className={navBarScroll ? 'nav-color' : 'nav-menu'}>
+        <ul>
+          <li className="nav-items">Home</li>
+          <li className="nav-items">About</li>
+          <li className="nav-items">Service</li>
+          <li className="nav-items">Career</li>
+          <li className="nav-items">Video</li>
+          <li className="nav-items">Team</li>
+          <li className="nav-items">Testimonial</li>
+          <li className="nav-items">Feedback</li>
+          <li className="nav-items">Contact</li>
+        </ul>
+      </nav>
+    </>;
+};
 
 export default Navbar;
