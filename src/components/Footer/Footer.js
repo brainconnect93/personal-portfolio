fa56dc732748a61e4027ff718ac596ddcc483add
brainconnect93 <brainconnect0@gmail.com>
2022-08-16 12:07:25 +0100

    Add the footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="container-fluid footer-section">
+      <div className="container">
+        <div className="row">
+          
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12"></div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12"></div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12"></div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
