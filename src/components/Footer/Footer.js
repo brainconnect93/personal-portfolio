dd86512dc2629ffdc3e53b7b41f821625eb1064c
brainconnect93 <brainconnect0@gmail.com>
2022-08-16 23:41:09 +0100

    start the first footer-details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,31 @@
 /* eslint-disable */
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
     <div className="container-fluid footer-section">
       <div className="container">
         <div className="row footer-content">
-          
           <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            Footer
+            <div className="footer-deatils">
+              <h5>About</h5>
+              <p>
+                I am Afolabi Akorede a software developer, with years of
+                experience on JavaScript, React, Redux, Tailwind, Ruby, Ruby on
+                rail and many more. I can help you build a product, feature or
+                website Look through some of my work and experience! If you like
+                what you see and have a project you need to work on, dont
+                hestiate to contact me.
+              </p>
+            </div>
           </div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            Footer
-          </div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            Footer
-          </div>
-
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">Footer</div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">Footer</div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
