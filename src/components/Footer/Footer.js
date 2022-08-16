5556388eb6f3001da37cfc3193bca7614bc40631
brainconnect93 <brainconnect0@gmail.com>
2022-08-16 22:12:01 +0100

    start the footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,22 @@
 /* eslint-disable */
 import React from 'react';
+import './footer.css';
 
 const Footer = () => {
   return (
     <div className="container-fluid footer-section">
       <div className="container">
-        <div className="row">
+        <div className="row footer-content">
           
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12"></div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12"></div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12"></div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+            Footer
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+            Footer
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+            Footer
+          </div>
 
         </div>
       </div>
