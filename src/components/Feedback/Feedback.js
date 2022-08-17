13739c304401738dd410fe54b219ca2fb177c5e8
brainconnect93 <brainconnect0@gmail.com>
2022-08-17 22:32:41 +0100

    Add end point Api

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,12 +1,76 @@
 /* eslint-disable */
-import React from 'react';
+import React from "react";
 import Slider from "react-slick";
-import './feedback.css'
+import "./feedback.css";
 
 const Feedback = () => {
-  return (
-    <div>Feedback</div>
-  )
-}
+  const feedData = [
+    {
+      image:
+        "https://images.pexels.com/photos/2962144/pexels-photo-2962144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn and become certified",
+      name: "Andreas Ellegaard",
+      title: "Ruby Developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn and become certified",
+      name: "Anthony Trivet",
+      title: "React and Javascript developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn and become certified",
+      name: "David Kwasi",
+      title: "Django Developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2589409/pexels-photo-2589409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn and become certified",
+      name: "Jairo David",
+      title: "Ruby on rail developer",
+    },
+    {
+        image:
+          "https://images.pexels.com/photos/2962144/pexels-photo-2962144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content:
+          "Thanks to Microverse for given me the chance to learn and become certified",
+        name: "Andreas Ellegaard",
+        title: "Front-End Developer",
+      },
+      {
+        image:
+          "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content:
+          "Thanks to Microverse for given me the chance to learn and become certified",
+        name: "Anthony Trivet",
+        title: "Fullstack developer",
+      },
+      {
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content:
+          "Thanks to Microverse for given me the chance to learn and become certified",
+        name: "David Kwasi",
+        title: "PHP Developer",
+      },
+      {
+        image:
+          "https://images.pexels.com/photos/2589409/pexels-photo-2589409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content:
+          "Thanks to Microverse for given me the chance to learn and become certified",
+        name: "Jairo David",
+        title: "NodeJs developer",
+      },
+   ];
+  return <div>Feedback</div>;
+};
 
 export default Feedback;
