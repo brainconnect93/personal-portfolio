385d87a188a69438412515a49fe66ba6beae6913
brainconnect93 <brainconnect0@gmail.com>
2022-08-17 22:34:15 +0100

    Add more end point Api

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -69,6 +69,22 @@ const Feedback = () => {
         name: "Jairo David",
         title: "NodeJs developer",
       },
+      {
+        image:
+          "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content:
+          "Thanks to Microverse for given me the chance to learn and become certified",
+        name: "Anthony Trivet",
+        title: "Fullstack developer",
+      },
+      {
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content:
+          "Thanks to Microverse for given me the chance to learn and become certified",
+        name: "David Kwasi",
+        title: "PHP Developer",
+      },
    ];
   return <div>Feedback</div>;
 };
