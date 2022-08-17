8dc21947aaad2daf5c08b2748ff8d50d8225aadc
brainconnect93 <brainconnect0@gmail.com>
2022-08-17 02:23:52 +0100

    Finish all footer section and the styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 import React from "react";
 import { FaFacebookSquare,FaInstagramSquare,FaGithubSquare } from "react-icons/fa";
-import { BsTwitter, BsLinkedin } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { AiFillTwitterSquare } from "react-icons/ai";
 import "./footer.css";
 
 const Footer = () => {
@@ -10,22 +11,20 @@ const Footer = () => {
       <div className="container">
         <div className="row footer-content">
           <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            <div className="footer-deatils">
+            <div className="footer-about">
               <h5>About</h5>
               <p>
                 I am Afolabi Akorede a software developer, with years of
                 experience on JavaScript, React, Redux, Tailwind, Ruby, Ruby on
                 rail and many more. I can help you build a product, feature or
-                website Look through some of my work and experience! If you like
-                what you see and have a project you need to work on, dont
-                hestiate to contact me.
+                website.
               </p>
               <span className="footer-icons">
                 <p>
                   <FaFacebookSquare size={20} />
                 </p>
                 <p>
-                  <BsTwitter size={20} />
+                  <AiFillTwitterSquare size={22} />
                 </p>
                 <p>
                   <FaInstagramSquare size={20} />
@@ -39,8 +38,32 @@ const Footer = () => {
               </span>
             </div>
           </div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">Footer</div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">Footer</div>
+
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+            <div className="footer-service">
+              <h5>Service</h5>
+              <div className="footer-item">
+                <li>Website Design</li>
+                <li>Innovation</li>
+                <li>Technology</li>
+                <li>Media Marketing</li>
+                <li>Product Sort & Design</li>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+          <div className="footer-custom">
+              <h5>Customer Care</h5>
+              <div className="footer-item">
+                <li>Log In</li>
+                <li>My Account</li>
+                <li>Wish List</li>
+                <li>FAQs</li>
+                <li>Contact Us</li>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
