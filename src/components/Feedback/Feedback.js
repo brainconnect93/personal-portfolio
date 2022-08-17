3f47b4f88f9423566f91e4f7f9e67b479b6f12a4
brainconnect93 <brainconnect0@gmail.com>
2022-08-17 22:26:21 +0100

    add the Feedback folder aand files

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react';
+import Slider from "react-slick";
+import './feedback.css'
+
+const Feedback = () => {
+  return (
+    <div>Feedback</div>
+  )
+}
+
+export default Feedback;
