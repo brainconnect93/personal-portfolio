3173bee60f6dcd26647a3ac87e0aba1a0e63e2bf
brainconnect93 <brainconnect0@gmail.com>
2022-08-18 20:19:31 +0100

    Install react slick slider

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -11,7 +11,7 @@ const Feedback = () => {
       image:
         "https://images.pexels.com/photos/2962144/pexels-photo-2962144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       content:
-        "Thanks to Microverse for given me the chance to learn and become certified",
+        "Thanks to Microverse for given me the chance to learn",
       name: "Andreas Ellegaard",
       title: "Ruby Developer",
     },
@@ -19,7 +19,7 @@ const Feedback = () => {
       image:
         "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       content:
-        "Thanks to Microverse for given me the chance to learn and become certified",
+        "Thanks to Microverse for given me the chance to learn",
       name: "Anthony Trivet",
       title: "React and Javascript developer",
     },
@@ -27,7 +27,7 @@ const Feedback = () => {
       image:
         "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       content:
-        "Thanks to Microverse for given me the chance to learn and become certified",
+        "Thanks to Microverse for given me the chance to learn",
       name: "David Kwasi",
       title: "Django Developer",
     },
@@ -35,71 +35,71 @@ const Feedback = () => {
       image:
         "https://images.pexels.com/photos/2589409/pexels-photo-2589409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       content:
-        "Thanks to Microverse for given me the chance to learn and become certified",
+        "Thanks to Microverse for given me the chance to learn",
       name: "Jairo David",
       title: "Ruby on rail developer",
     },
     {
-        image:
-          "https://images.pexels.com/photos/2962144/pexels-photo-2962144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        content:
-          "Thanks to Microverse for given me the chance to learn and become certified",
-        name: "Andreas Ellegaard",
-        title: "Front-End Developer",
-      },
-      {
-        image:
-          "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        content:
-          "Thanks to Microverse for given me the chance to learn and become certified",
-        name: "Anthony Trivet",
-        title: "Fullstack developer",
-      },
-      {
-        image:
-          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        content:
-          "Thanks to Microverse for given me the chance to learn and become certified",
-        name: "David Kwasi",
-        title: "PHP Developer",
-      },
-      {
-        image:
-          "https://images.pexels.com/photos/2589409/pexels-photo-2589409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        content:
-          "Thanks to Microverse for given me the chance to learn and become certified",
-        name: "Jairo David",
-        title: "NodeJs developer",
-      },
-      {
-        image:
-          "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        content:
-          "Thanks to Microverse for given me the chance to learn and become certified",
-        name: "Anthony Trivet",
-        title: "Fullstack developer",
-      },
-      {
-        image:
-          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        content:
-          "Thanks to Microverse for given me the chance to learn and become certified",
-        name: "David Kwasi",
-        title: "PHP Developer",
-      },
-   ];
+      image:
+        "https://images.pexels.com/photos/2962144/pexels-photo-2962144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn",
+      name: "Andreas Ellegaard",
+      title: "Front-End Developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn",
+      name: "Anthony Trivet",
+      title: "Fullstack developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn",
+      name: "David Kwasi",
+      title: "PHP Developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2589409/pexels-photo-2589409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn",
+      name: "Jairo David",
+      title: "NodeJs developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/884422/pexels-photo-884422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn",
+      name: "Anthony Trivet",
+      title: "Fullstack developer",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      content:
+        "Thanks to Microverse for given me the chance to learn",
+      name: "David Kwasi",
+      title: "PHP Developer",
+    },
+  ];
 
-   var settings = {
+  var settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    autoPlay: true,
-    slidesPerRow: 2,
-    row: 1,
-    initialSlide: 0,
+    slidesToShow: 2,
     adaptiveHeight: true,
-    slidesToScroll: 1
+    initialSlide: 0,
+    autoPlay: true,
+    slidesPerRow: 1,
+    row: 1,
+    // slidesToScroll: 1
   };
   return (
     <div className="container feed-section" id="feedback">
@@ -108,21 +108,24 @@ const Feedback = () => {
         <span className="line"></span>
       </div>
 
-      <Slider {...settings}>
       <div className="feed-content">
-        {feedData.map((item,index) => (
-          <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-            <div className="card feed-slider" key={index}>
-              <img src={item.image} alt="feedback-image" className="feed-img" />
-              <p>{item.content}</p>
-              <h6>{item.name}</h6>
-              <p>{item.title}</p>
+        <Slider {...settings}>
+          {feedData.map((item, index) => (
+            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+              <div className="card feed-slider" key={index}>
+                <img
+                  src={item.image}
+                  alt="feedback-image"
+                  className="feed-img"
+                />
+                <p>{item.content}</p>
+                <h6>{item.name}</h6>
+                <p>{item.title}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
-      </Slider>
-
     </div>
   );
 };
